c38bede72e7f18321851b06797bf42a211bb2b84
johnxv <john9sea@gmail.com>
2021-01-04 19:40:53 -0700

    Created 2 different functions: sayHello() and sayHello2(). sayHello() only prints out Hello, world while sayHello2() accepts an argument called name and prints Hello, name

---
+++ hello.js
@@ -0,0 +1,20 @@
+// simple function...
+console.log("sayHello();");
+const sayHello = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+console.log();
+
+// revised sayHello() function with more functionality.
+console.log("sayHello2();");
+const sayHello2 = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello2("John");
+sayHello2("Doe");
+sayHello2("Jane");
+
